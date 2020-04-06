4364d9a84a05aa84b438b6e6e68fc8178c6227c5
y1rks <yu.martian@gmail.com>
2020-04-06 13:17:13 +0900

    Add q20 21

--- chapter3/q20.py
+++ chapter3/q20.py
@@ -9,3 +9,15 @@ Wikipediaの記事を以下のフォーマットで書き出したファイルja
 20. JSONデータの読み込み
 Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．問題21-29では，ここで抽出した記事本文に対して実行せよ．
 '''
+import gzip
+import json
+
+def getBritishText():
+  with gzip.open('jawiki-country.json.gz', 'rt') as f:
+    for line in f.readlines():
+      json_file = json.loads(line)
+      if json_file['title'] == 'イギリス':
+        return json_file['text']
+
+if __name__ == '__main__':
+  print(getBritishText())
--- chapter3/q21.py
+++ chapter3/q21.py
@@ -2,3 +2,11 @@
 21. カテゴリ名を含む行を抽出
 記事中でカテゴリ名を宣言している行を抽出せよ．
 '''
+
+import q20
+
+text = q20.getBritishText()
+
+for line in text.split('\n'):
+  if 'Category' in line:
+    print(line)
