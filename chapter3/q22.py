0916e025a6fb8dbc17cfd1ae0ce67e33399bf04e
y1rks <yu.martian@gmail.com>
2020-04-07 01:16:06 +0900

    Add q22

--- chapter3/q22.py
+++ chapter3/q22.py
@@ -2,3 +2,10 @@
 22. カテゴリ名の抽出
 記事のカテゴリ名を（行単位ではなく名前で）抽出せよ．
 '''
+
+import q20
+import re
+
+text = q20.getBritishText()
+patterns = re.findall(r'^\[\[Category:(.+?)(?:\|.*)?\]\]$', text, flags=re.M)
+print(patterns)
