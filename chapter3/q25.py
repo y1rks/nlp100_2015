fc3b12776f9c82e8618f1470f6061ae80147ce17
y1rks <yu.martian@gmail.com>
2020-04-15 01:33:26 +0900

    Add q25

--- chapter3/q25.py
+++ chapter3/q25.py
@@ -1,5 +1,32 @@
 '''
-
 25. テンプレートの抽出
 記事中に含まれる「基礎情報」テンプレートのフィールド名と値を抽出し，辞書オブジェクトとして格納せよ．
+
+{{基礎情報 国
+|略名 = イギリス
+|日本語国名 = グレートブリテン及び北アイルランド連合王国
+|公式国名 = {{lang|en|United Kingdom of Great Britain and Northern Ireland}}<ref>英語以外での正式国名:<br/>
+*{{lang|gd|An Rìoghachd Aonaichte na Breatainn Mhòr agus Eirinn mu Thuath}}（[[スコットランド・ゲール語]]）<br/>
+*{{lang|cy|Teyrnas Gyfunol Prydain Fawr a Gogledd Iwerddon}}（[[ウェールズ語]]）<br/>
+*{{lang|ga|Ríocht Aontaithe na Breataine Móire agus Tuaisceart na hÉireann}}（[[アイルランド語]]）<br/>
+*{{lang|kw|An Rywvaneth Unys a Vreten Veur hag Iwerdhon Glédh}}（[[コーンウォール語]]）<br/>
+*{{lang|sco|Unitit Kinrick o Great Breetain an Northren Ireland}}（[[スコットランド語]]）<br/>
+**{{lang|sco|Claught Kängrick o Docht Brätain an Norlin Airlann}}、{{lang|sco|Unitet Kängdom o Great Brittain an Norlin Airlann}}（アルスター・スコットランド語）</ref>
+...
+
 '''
+import q20, re
+
+text = q20.getBritishText()
+
+# テンプレート抽出
+template = re.findall(r'^\{\{基礎情報.*?$(.*?)^\}\}$', text, re.M + re.DOTALL)
+
+# キーと値を取得
+patterns = re.findall(r'^\|(.+?)\s*=\s*(.+?)(?:(?=\n\|)|(?=\n$))', template[0], re.M + re.DOTALL)
+
+result = {}
+for pattern in patterns:
+    result[pattern[0]] = pattern[1]
+
+print(result)
