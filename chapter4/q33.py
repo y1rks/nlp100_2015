fb434531072d4b7d71608054e0782182f12a6ed7
y1rks <yu.martian@gmail.com>
2020-05-04 16:10:59 +0900

    Add q33

--- chapter4/q33.py
+++ chapter4/q33.py
@@ -2,3 +2,15 @@
 33. 「AのB」
 2つの名詞が「の」で連結されている名詞句を抽出せよ．
 '''
+
+import q30
+
+data = q30.mapper()
+lastIndex = len(data) - 1
+result = []
+for i in range(lastIndex + 1):
+    if data[i]['surface'] == 'の' and i != 0 and i != lastIndex:
+        if data[i - 1]['pos'] == '名詞' and data[i + 1]['pos'] == '名詞':
+            result.append(data[i - 1]['surface'] + data[i]['surface'] + data[i + 1]['surface'])
+
+print(result)
