bbab7a7ec6a8a3be94a47ce86c11042d6b2b6cc6
y1rks <yu.martian@gmail.com>
2020-05-07 19:35:03 +0900

    Add 35

--- chapter4/q35.py
+++ chapter4/q35.py
@@ -2,3 +2,17 @@
 35. 単語の出現頻度
 文章中に出現する単語とその出現頻度を求め，出現頻度の高い順に並べよ．
 '''
+import q30
+
+data = q30.mapper()
+words = {}
+
+for word in data:
+    if word['surface'] in words:
+        words[word['surface']] += 1
+    else:
+        words[word['surface']] = 1
+
+result = sorted(words.items(), key=lambda x: x[1], reverse=True)
+
+print(result)
