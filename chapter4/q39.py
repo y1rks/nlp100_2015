cd0d16b85b2ba98b871a0a5173de90e14bd1b6b6
y1rks <yu.martian@gmail.com>
2020-05-09 00:35:05 +0900

    Add 39

--- chapter4/q39.py
+++ chapter4/q39.py
@@ -2,3 +2,30 @@
 39. Zipfの法則
 単語の出現頻度順位を横軸，その出現頻度を縦軸として，両対数グラフをプロットせよ．
 '''
+import q30
+import matplotlib.pyplot as plt
+
+data = q30.mapper()
+words = {}
+
+for word in data:
+    if word['surface'] in words:
+        words[word['surface']] += 1
+    else:
+        words[word['surface']] = 1
+
+result = sorted(words.items(), key=lambda x: x[1], reverse=True)
+
+x = []
+y = []
+
+count = 1
+for word in result:
+    x.append(count)
+    y.append(word[1])
+    count += 1
+
+plt.plot(x, y)
+plt.xscale('log')
+plt.yscale('log')
+plt.show()
