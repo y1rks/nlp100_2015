2a825e1314259f2903b644957e0c65583632f98a
y1rks <yu.martian@gmail.com>
2020-05-08 14:32:48 +0900

    Add 36

--- chapter4/q36.py
+++ chapter4/q36.py
@@ -2,3 +2,26 @@
 36. 頻度上位10語
 出現頻度が高い10語とその出現頻度をグラフ（例えば棒グラフなど）で表示せよ．
 '''
+import q30
+import matplotlib.pyplot as plt
+
+data = q30.mapper()
+words = {}
+
+for word in data:
+    if word['surface'] in words:
+        words[word['surface']] += 1
+    else:
+        words[word['surface']] = 1
+
+result = sorted(words.items(), key=lambda x: x[1], reverse=True)[:10]
+
+x = []
+y = []
+
+for word in result:
+    x.append(word[0])
+    y.append(word[1])
+
+plt.bar(x, y)
+plt.show()
