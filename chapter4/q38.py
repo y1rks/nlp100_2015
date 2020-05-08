e85ffddd2ec593f417bd698948a46a6057081789
y1rks <yu.martian@gmail.com>
2020-05-08 23:56:58 +0900

    Add 38

--- chapter4/q38.py
+++ chapter4/q38.py
@@ -2,3 +2,23 @@
 38. ヒストグラム
 単語の出現頻度のヒストグラム（横軸に出現頻度，縦軸に出現頻度をとる単語の種類数を棒グラフで表したもの）を描け．
 '''
+import q30
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = q30.mapper()
+words = {}
+
+for word in data:
+    if word['surface'] in words:
+        words[word['surface']] += 1
+    else:
+        words[word['surface']] = 1
+
+freqs = []
+
+for key in words:
+    freqs.append(words[key])
+
+plt.hist(freqs, bins=50, range=(1, 50))
+plt.show()
